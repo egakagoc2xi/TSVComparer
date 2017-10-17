5a24a7f8a837891d1c88e8293783e08021fcc931
eduardojgonzaleza@gmail.com <16092006@Emar>
2017-10-17 09:12:06 -0500

    New Project

---
+++ TSVComparer.Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSVComparer.Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
